952e1f62dc17605fa7516379eb9c965e6f87b44d
SilviaIzmi <silviaizmi67@gmail.com>
2022-12-18 22:37:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasservice_2018099/DestinationActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugasservice_2018099;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
